b30b7c05b90974dce4dcb7de90fe19d12a299b03
Lucho <luciano@wisboo.com>
2023-12-13 06:51:25 -0300

    add redis password

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,12 @@ import RedisClient from 'ioredis'
 
 const redisHost = process.env.REDIS_HOST ?? 'localhost'
 const redisPort = process.env.REDIS_PORT ?? '6379'
+const redisPassword = process.env.REDIS_PASSWORD ?? 'password'
 
 const redis = new RedisClient({
   host: redisHost,
-  port: Number(redisPort)
+  port: Number(redisPort),
+  password: redisPassword
 })
 
 const Data: RoomsData = config
